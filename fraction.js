518d2964f76da4f88cf505731b4740577c160205
chayanikachoudhary1 <115769392+chayanikachoudhary1@users.noreply.github.com>
2023-04-20 11:52:49 +0530

    subtraction

--- fraction.js
+++ fraction.js
@@ -1,1 +1,12 @@
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
 
+  subtract(otherFraction) {
+    const newNumerator = this.numerator * otherFraction.denominator - otherFraction.numerator * this.denominator;
+    const newDenominator = this.denominator * otherFraction.denominator;
+    return new Fraction(newNumerator, newDenominator);
+  }
+}
