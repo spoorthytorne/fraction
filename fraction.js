b601056e55d0b54f8c2c3c48e07c679939eef061
spoorthytorne <114483219+spoorthytorne@users.noreply.github.com>
2023-04-20 11:43:39 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,29 @@
 
+#class add
+
+const prompt = require('prompt-sync')();
+
+function input()
+
+{
+  a=parseInt(prompt("enter the number"));
+  b=parseInt(prompt("enter the another number"));
+return[a,b];
+  
+}
+function add(a,b)
+{
+  return a+b;
+}
+function output(a,b,c)
+{
+  console.log("the sum of %d %d is %d",a,b,c)
+}
+function main()
+{
+  let a,b;
+  [a,b]=input();
+  let c=add(a,b);
+  output(a,b,c);
+}
+main()
