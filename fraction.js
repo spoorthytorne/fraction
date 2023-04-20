38a9843c2a9aecc57711e8e7e85190485e6ea9aa
spoorthytorne <114483219+spoorthytorne@users.noreply.github.com>
2023-04-20 11:50:49 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,29 +1,12 @@
-
-#class add
-
-const prompt = require('prompt-sync')();
-
-function input()
-
-{
-  a=parseInt(prompt("enter the number"));
-  b=parseInt(prompt("enter the another number"));
-return[a,b];
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
   
+  add(other) {
+    const newNumerator = this.numerator * other.denominator + this.denominator * other.numerator;
+    const newDenominator = this.denominator * other.denominator;
+    return new Fraction(newNumerator, newDenominator);
+  }
 }
-function add(a,b)
-{
-  return a+b;
-}
-function output(a,b,c)
-{
-  console.log("the sum of %d %d is %d",a,b,c)
-}
-function main()
-{
-  let a,b;
-  [a,b]=input();
-  let c=add(a,b);
-  output(a,b,c);
-}
-main()
