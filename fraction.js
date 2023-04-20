36a413661291817687246a23549a2698bc8c4e5b
Priyanshuray <114483180+Priyanshuray@users.noreply.github.com>
2023-04-20 11:54:36 +0530

    division

--- fraction.js
+++ fraction.js
@@ -1,1 +1,12 @@
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
 
+  divide(other) {
+    const numerator = this.numerator * other.denominator;
+    const denominator = this.denominator * other.numerator;
+    return new Fraction(numerator, denominator);
+  }
+}
